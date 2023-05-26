f585a6763e830a1623e7952432af71890989632f
pravinui <110659009+pravinui@users.noreply.github.com>
2023-05-26 23:36:21 +0530

    [jyoti] :new class ex created

---
+++ src/Action_Class/EX.java
@@ -0,0 +1,15 @@
+package Action_Class;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EX 
+{
+
+	public static void main(String[] args) 
+	{
+		 System.out.println("Hi");
+		 System.out.println("Hello");
+		
+	}
+}
